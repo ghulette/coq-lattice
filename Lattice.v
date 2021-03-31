ad4a1daaf54ff98b9c6f07c3867c82db85a1eb10
Geoff Hulette <geoff@hulette.net>
2021-03-31 14:26:02 -0700

    Update

--- Lattice.v
+++ Lattice.v
@@ -4,15 +4,52 @@ Section Relations.
 
   Definition Rel := A -> A -> Prop.
 
-  Definition reflexive (R : Rel) := forall x, R x x.
+  Variable R : Rel.
 
-  Definition symmetric (R : Rel) := forall x y, R x y -> R y x.
+  Definition reflexive := forall x, R x x.
 
-  Definition antisymmetric (R : Rel) := forall x y, R x y -> R y x -> x = y.
+  Definition symmetric := forall x y, R x y -> R y x.
 
-  Definition transitive (R : Rel) := forall x y z, R x y -> R y z -> R x z.
+  Definition antisymmetric := forall x y, R x y -> R y x -> x = y.
+
+  Definition transitive := forall x y z, R x y -> R y z -> R x z.
+
+  Definition order := reflexive /\ antisymmetric /\ transitive.
 
 End Relations.
+
+Section Lattice.
+
+  Variable A : Type.
+  Variable R : Rel A.
+  Hypothesis ordered : order A R.
+
+  Definition upper_bound x y ub := R x ub /\ R y ub.
+
+  Definition lub x y ub :=
+    upper_bound x y ub /\
+    forall ub', upper_bound x y ub' -> R ub ub'.
+
+  Theorem lub_unique :
+    forall x y, exists z, lub x y z -> unique (lub x y) z.
+  Admitted.
+
+  Definition lower_bound x y lb := R lb x /\ R lb y.
+
+  Definition glb x y lb :=
+    lower_bound x y lb /\
+    forall lb', lower_bound x y lb' -> R lb' lb.
+
+  Theorem glb_unique :
+    forall x y, exists z, glb x y z -> unique (glb x y) z.
+  Admitted.
+
+  Definition lattice :=
+    forall x y,
+      (exists u, lub x y u) /\
+      (exists l, glb x y l).
+
+End Lattice.
 
 
 Section Ensembles.
@@ -25,7 +62,7 @@ Section Ensembles.
 
   Definition included (s1 s2: Ensemble) := forall x, s1 x -> s2 x.
 
-  Inductive empty : Ensemble := .
+  Variant empty : Ensemble := .
 
   Definition is_empty (s : Ensemble) := ~exists x, s x.
 
